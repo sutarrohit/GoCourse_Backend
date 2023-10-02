36ffdc8be9b3440e0d639cdd049e6ef61063783d
sutarrohit <rohitsutar082@gmail.com>
2023-10-02 17:42:53 +0530

    fix:Docker compose file

--- dist/Utils/catchAsync.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const catchAsync = (myFn) => {
-    return (req, res, next) => {
-        myFn(req, res, next).catch((error) => next(error));
-    };
-};
-exports.default = catchAsync;
